825d1c095316b003e3313c2c3b8bef18f21fd0b7
Sammwy <44925968+sammwyy@users.noreply.github.com>
2019-11-26 23:19:31 -0300

    Update score.py

--- score.py
+++ score.py
@@ -1,6 +1,9 @@
+# Import the Pygame library
 import pygame
 
+# Define class score for easy imporit in the main.py
 class Score (pygame.sprite.Sprite):
+	# Define main function
 	def __init__ (self, font, pos = (0, 0)):
 		pygame.sprite.Sprite.__init__(self)
 		self.font = font
@@ -9,9 +12,11 @@ class Score (pygame.sprite.Sprite):
 		self.image = self.font.render(str(self.score), 0, (255, 255, 255))
 		self.rect = self.image.get_rect(center = self.pos)
 
+	# Add 1+ point to the score
 	def score_up (self):
 		self.score += 1
 
+	# Update sprite
 	def update(self):
 		self.image = self.font.render(str(self.score), 0, (255, 255, 255))
 		self.rect = self.image.get_rect(center = self.pos)
