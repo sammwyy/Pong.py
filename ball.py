bc782f46275ea43ba2a64b02488d0d24f5179ff7
Sammwy <44925968+sammwyy@users.noreply.github.com>
2019-11-26 23:17:15 -0300

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,7 +1,11 @@
+# Import the Pygame library
 import pygame
 
+# Define class ball for easy imporit in the main.py
 class Ball (pygame.sprite.Sprite):
+	# Define init function (main)
 	def __init__ (self, pos = (0, 0)):
+		# Create sprite
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.Surface ((20, 20)).convert()
 		self.pos = pos
@@ -10,22 +14,28 @@ class Ball (pygame.sprite.Sprite):
 		self.speed_x = 0
 		self.speed_y = 0
 
+	# Invert Y movement
 	def change_y (self):
 		self.speed_y *= -1
 
+	# Invert X movement
 	def change_x (self):
 		self.speed_x *= -1
 
+	# Start ball movement
 	def start (self, speed_x, speed_y):
 		self.speed_x = speed_x
 		self.speed_y = speed_y
 
+	# Stop ball movement
 	def stop (self):
 		self.speed_x = 0
 		self.speed_y = 0
 
+	# Reset ball position
 	def reset (self):
 		self.rect = self.image.get_rect (center = self.pos)
 
+	# Update ball position
 	def update (self):
 		self.rect.move_ip (self.speed_x, self.speed_y)
