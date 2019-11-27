8ad9964880d5069a974476a3f5d964cafcef5c3c
Sammwy <44925968+sammwyy@users.noreply.github.com>
2019-11-26 23:18:31 -0300

    Update pad.py

--- pad.py
+++ pad.py
@@ -1,7 +1,11 @@
+# Import the Pygame library
 import pygame
 
+# Define class pad for easy imporit in the main.py
 class Pad (pygame.sprite.Sprite):
+	# define main function
 	def __init__ (self, pos = (0, 0)):
+		# Create sprite
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.Surface((20, 100)).convert()
 		self.image.fill((255, 255, 255))
@@ -9,14 +13,18 @@ class Pad (pygame.sprite.Sprite):
 		self.max_speed = 10
 		self.speed = 0
 
+	# Move up pad
 	def move_up (self):
 		self.speed = self.max_speed * -1
 
+	# Move down pad
 	def move_down (self):
 		self.speed = self.max_speed * 1
 
+	# Stop pad movement
 	def stop (self):
 		self.speed = 0
 
+	# Update pad movement
 	def update (self):
 		self.rect.move_ip (0, self.speed)
